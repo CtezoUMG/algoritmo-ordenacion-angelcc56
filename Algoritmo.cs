014169e536aeab22648cb88f6a5f558693bc899a
angelcc56 <Icabrerag8@miumg.edu.gt>
2026-02-28 08:07:44 -0600

    implementando BubbleSort

--- Algoritmo.cs
+++ Algoritmo.cs
@@ -24,7 +24,22 @@ public class Algoritmo
 
     public void BubbleSort(int[] arr)
     {
-        // TODO: Implementar el algoritmo de Bubble Sort tradicional
-
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
     }
 }
